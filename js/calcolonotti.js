0662f17e0d0719410ea0c4caf5eda00c4e1a52db
nonnonsky <131236654+nonnonsky@users.noreply.github.com>
2025-04-03 07:02:55 +0200

    Create calcolonotti.js

---
+++ js/calcolonotti.js
@@ -0,0 +1,21 @@
+function calcolaNotti() {
+    var checkin = document.getElementById('checkin').value;
+    var checkout = document.getElementById('checkout').value;
+
+    if (checkin && checkout) {
+        // Converti le stringhe in oggetti Date
+        var date1 = new Date(checkin + "T00:00:00"); 
+        var date2 = new Date(checkout + "T00:00:00"); 
+
+        // Calcola la differenza in millisecondi
+        var diffTime = date2.getTime() - date1.getTime();
+        var diffDays = Math.floor(diffTime / (1000 * 3600 * 24)); // Converte in giorni interi
+
+        // Evita valori negativi
+        if (diffDays > 0) {
+            document.getElementById('totale-notti').innerText = diffDays + " notti";
+        } else {
+            document.getElementById('totale-notti').innerText = "Errore: Check-out deve essere dopo il Check-in";
+        }
+    }
+}
